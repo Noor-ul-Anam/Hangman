95a14b70d83a690dba082d89c85b7049e34cbb08
Noor-ul-Anam <anam.xyed97@gmail.com>
2019-04-29 23:25:40 +0500

    Uploading initial files

---
+++ js/js.js
@@ -0,0 +1,16 @@
+let alphabetArr = [];
+
+let keyboardAlphabet= () => {
+   let element = document.getElementById('keyboard');
+   // element.innerHTML = '';
+   let countForAscii = 65;
+   for (let i = 0; i < 26; i++) {
+      let alphabet = () => {
+         alphabetArr.push(String.fromCharCode(countForAscii));
+         countForAscii++;
+      }
+      alphabet();
+      element.innerHTML += `<div class="wordKB" id="${alphabetArr[i]}">`+`<p>${alphabetArr[i]}</p>`+`</div>`;
+   }
+}
+keyboardAlphabet();
