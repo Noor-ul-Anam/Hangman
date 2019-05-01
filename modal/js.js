f87986cdc5724305b05dae7e2ccb109b2c8df50a
Noor-ul-Anam <anam.xyed97@gmail.com>
2019-05-02 04:52:11 +0500

    modal component

---
+++ modal/js.js
@@ -0,0 +1,6 @@
+let modalAppear =() => {
+    document.getElementById('modal').style.visibility = 'visible';
+}
+let closeModal= () =>{
+    document.getElementById('modal').style.visibility = 'hidden';
+}
